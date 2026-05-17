b0fc377d0cf759c5ddb36831e64a9e697081df3a
pcodespace <techshow129129@gmail.com>
2026-05-17 01:02:07 +0000

    Finished Lession 2

---
+++ lesson2/classwork/casting.py
@@ -0,0 +1,19 @@
+num_str=  "42"
+num_int = int(num_str)      #Convert string to an integer
+print(num_int  + 1)
+
+float_str   = "3.14"
+num_float   =   float(float_str)    # Convert string to float
+print(num_float + 2.19)
+
+int_num=7
+float_num=float(int_num)    # Convert integer to floar
+print(float_num)
+
+float_num2 = 9.99
+int_num2= int(float_num2)  #Convert float to integer (doesn't road, truncates decimal)
+print(int_num)
+
+num = 100
+num_str = str(num)  #Convert integer to string
+print(num_str)
---
+++ lesson2/classwork/math_operators.py
@@ -0,0 +1,27 @@
+print(4 + 5)   #Addition:   adds two numbers
+
+print(10 - 5)   #Subtraction: subtracts the second number from the first number
+
+print(6 * 7)    # Multiplication: multiplies two numbers
+
+print(15 / 2)   #Division: divides and gives a float result
+
+print(15//2) #Floor division: divides and rounds down to a whole number
+
+print(15 % 2)
+
+print (2**3)    #Expoentiation: first number raised to the power of second number
+
+#You can combine numbers and math operations
+# Python follows the order of operations
+print(2 + 3*4)
+print((2+3)*4)
+
+a=8
+b=3
+print("a+b=",a*b)
+print("a*b=",a*b)
+
+x=int(input("Enter a number : " ))
+y=float(input("Enter another number:    "))
+print("Sum is",x+y)
