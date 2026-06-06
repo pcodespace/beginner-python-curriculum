57be1a66ae706c68740486ec0815c6f7882e9618
pcodespace <techshow129129@gmail.com>
2026-06-06 05:55:06 +0000

    Finished L4HW4

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 #from lesson1.classwork import printing
 #from lesson2.homework import homework
-from lesson3.homework import problems
+#from lesson3.homework import problems
+from lesson4.homework import problems
